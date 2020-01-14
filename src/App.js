2fd469333c8b3e9d6e4dd802f34f0b57f50bbc5a
Aquila Afuadajo <aquilaafuadajo@gmail.com>
2020-01-14 13:45:07 +0100

    adding project folder for article-viewer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from './components/Header/header.component'
+
+import './App.css';
+import Hompage from './pages/homepage/homepage.component';
+
+class App extends Component {
+  state = { 
+      url: `https://newsapi.org/v2/top-headlines?language=en&category=entertainment&apiKey=8a8c2162bdf243d48e6e87b8215df996`
+    }
+
+  render() { 
+    return (
+      <div className='App'>
+        <Header />
+        <Hompage />
+      </div>
+      );
+  }
 }
 
 export default App;
---
+++ src/components/Header/header.component.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './header.styles.scss';
+
+const Header = () => {
+  return ( 
+    <div className='header'>
+      <p>GENERAL</p>
+      <p>ENTERTAINMENT</p>
+      <p>BUSINESS</p>
+      <p>HEALTH</p>
+      <p>SCIENCE</p>
+      <p>SPORTS</p>
+      <p>TECHNOLOGY</p>
+    </div>
+    );
+}
+
+export default Header;
