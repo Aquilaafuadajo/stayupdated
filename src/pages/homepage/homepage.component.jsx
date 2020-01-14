2aec85567094f238c0fb218965fe38032f5750b4
Aquila Afuadajo <aquilaafuadajo@gmail.com>
2020-01-15 00:09:54 +0100

    designed the homepage component to display data fetched from the api, and added a spinning loader

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,19 +1,87 @@
 import React from 'react';
 
 import Async from 'react-async';
+import spinner from '../../assets/Spinner.svg';
+
+import './homepage.styles.scss';
 
 const api = (category) => `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=8a8c2162bdf243d48e6e87b8215df996`;
 
-fetch("https://newsapi.org/v2/top-headlines?language=en&country=us&category=entertainment&apiKey=8a8c2162bdf243d48e6e87b8215df996").then(response => response.json()).then(response => console.table(response.articles))
+fetch("https://newsapi.org/v2/top-headlines?language=en&country=us&category=entertainment&apiKey=8a8c2162bdf243d48e6e87b8215df996").then(response => response.json()).then(response => console.log(response.articles))
 
 const categories = ['business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology'];
 
 const categoriesApi = categories.map((category) => api(category))
                                 .map((categoryApi) => 
                                 () => (fetch(categoryApi)
-                                  .then(res => (res.ok ? res : Promise.reject(res)))
-                                  .then(res => res.json())))
+                                        .then(res => (res.ok ? res : Promise.reject(res)))
+                                        .then(res => res.json())))
 
+const Hompage = () => {
+  return ( 
+      <div className='homepage'>
+        {
+          categoriesApi.map((category, i) => (
+            <Async className='homepage' key={i} promiseFn={category}>
+              {
+                ({ data, err, isLoading }) => {
+                  if (isLoading) return (<img src={spinner} className="App-logo" alt="logo" />)
+                  if (err) return <h1>{`Something went wrong: ${err.message}`}</h1>
+
+                  if (data) return(
+                    data.articles.map(({urlToImage, author, description }, i) => {
+                      if (i<5)
+                      return (
+                        <div className='news-card' key={i}>
+                          <img src={urlToImage} alt="img" />
+                          <p>By: {author}</p>
+                          <p>{description}</p>
+                        </div>
+                      )
+                      }))
+                }
+              }
+            </Async>
+          ))
+        }
+      </div>
+    );
+}
+
+export default Hompage;
+
+
+
+
+
+
+
+
+
+
+
+// <Async promiseFn={news}>
+//         {({ data, err, isLoading }) => {
+//           if (isLoading) return (<img src={logo} className="App-logo" alt="logo" />)
+//           if (err) return <h1>{`Something went wrong: ${err.message}`}</h1>
+
+//           if(data) return (
+//             <div className='content'>
+//               {
+//                 data.articles.map(({source: {id, name} }, i) => {
+//                   if(i<5) {
+//                     return(<h1 className='box1' key={i}>{`${id}   ${name}`}</h1>)
+//                   }
+//                   if(i>5){
+//                     return(<h1 className='box' key={i}>{`${id}   ${name}`}</h1>)
+//                   }
+//                   return console.log(i>5)
+//                 })
+//               }
+//             </div>
+//           )}
+//         }
+//       </Async>
 
 // const news = () =>
 //   fetch("https://newsapi.org/v2/top-headlines?country=ng&category=entertainment&apiKey=8a8c2162bdf243d48e6e87b8215df996")
@@ -30,36 +98,3 @@ const categoriesApi = categories.map((category) => api(category))
 // console.log(actualNews[1].then(res => console.log(res.articles)))
 
 // console.log(categoryArray)
-
-
-
-const Hompage = () => {
-  return ( 
-      <div className='homepage'>
-        <Async promiseFn={categoriesApi[0]}>
-        {({ data, err, isLoading }) => {
-          if(isLoading) return ('loading...')
-          if(err) return (<h1>{`Something went wrong: ${err.message}`}</h1>)
-
-          if(data) return (
-            <div className='content'>
-              {
-                data.articles.map(({source: {id, name} }, i) => {
-                  if(i<5) {
-                    return(<h1 className='box1' key={i}>{`${id}   ${name}`}</h1>)
-                  }
-                  if(i>5){
-                    return(<h1 className='box' key={i}>{`${id}   ${name}`}</h1>)
-                  }
-                  return console.log(i>5)
-                })
-              }
-            </div>
-          )
-        }}
-        </Async>
-      </div>
-    );
-}
-
-export default Hompage;
